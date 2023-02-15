e3f4f6b1f5859288f3b921fda37288ae11533e56
Pobitrodeb <pobitrodn@gmail.com>
2023-02-15 21:11:16 +0600

    prams pass

--- 01_Arrow Functions/index.js
+++ 01_Arrow Functions/index.js
@@ -9,3 +9,12 @@ let number = () =>
 
 }
 console.log(number());
+
+// param pass  
+let userInfo = (name, email) => 
+{
+   this.name    = name;
+   this.email   = email;
+}; 
+
+console.log('Pobitro Debnath', 'pobitrodn@gmail.com');
