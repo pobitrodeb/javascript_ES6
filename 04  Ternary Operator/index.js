3b8aa972cd6b1235e64561d7ced7e54c48978404
Pobitrodeb <pobitrodn@gmail.com>
2023-02-16 21:49:00 +0600

    declaer ternary operator

---
+++ 04  Ternary Operator/index.js
@@ -0,0 +1,13 @@
+var age = 11; 
+
+var type; 
+
+// if( age >= 18){
+//     type = 'adult';
+// }else {
+//     type = 'child';
+// }
+
+// console.log(type);
+
+var type = (age >= 18) ? 'adult' : 'chaild'; 
