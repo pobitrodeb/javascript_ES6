50d49163f8c7d269d35f1ac36ff424306712e5e5
Pobitrodeb <pobitrodn@gmail.com>
2023-02-17 00:48:52 +0600

    multi array concat

--- 10 Array concat/index.js
+++ 10 Array concat/index.js
@@ -1,6 +1,7 @@
 var number1 = [1,2,3,4]; 
 var number2 = [5,6,7,8]; 
+var number3 = [9,10,11,12]; 
 
-var result = number1.concat(number2);
+var result = number1.concat(number2, number3);
 
 console.log(result);
