aafd21195c099dbf3125029f718546c015d03a97
Pobitrodeb <pobitrodn@gmail.com>
2023-02-16 22:15:18 +0600

    inventroy array find

--- 05 Array/index.js
+++ 05 Array/index.js
@@ -1,4 +1,28 @@
+// Arrow function
+// find((element) => { /* … */ })
+// find((element, index) => { /* … */ })
+// find((element, index, array) => { /* … */ })
+const inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
+];
 
+function isCherries(fruit) {
+  return fruit.name === "cherries";
+}
+
+console.log(inventory.find(isCherries));
+// { name: 'cherries', quantity: 5 }
+// // Callback function
+// find(callbackFn)
+// find(callbackFn, thisArg)
+
+// // Inline callback function
+// find(function (element) { /* … */ })
+// find(function (element, index) { /* … */ })
+// find(function (element, index, array) { /* … */ })
+// find(function (element, index, array) { /* … */ }, thisArg)
 
 var rolls = [1,2,3,4,5,6]
 var result = rolls.find(function(currentValue){
@@ -6,3 +30,16 @@ var result = rolls.find(function(currentValue){
 });
 
 console.log(result);
+
+const inventory = [
+    { name: "apples", quantity: 2 },
+    { name: "bananas", quantity: 0 },
+    { name: "cherries", quantity: 5 },
+  ];
+  
+  function isCherries(fruit) {
+    return fruit.name === "cherries";
+  }
+  
+  console.log(inventory.find(isCherries));
+
