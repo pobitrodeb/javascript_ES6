f167d4299038b5ee293c57b0251602c5e12f2a2b
Pobitrodeb <pobitrodn@gmail.com>
2023-02-17 03:41:51 +0600

    Spread Operator uses

---
+++ 16 Function default paramete/index.js
@@ -0,0 +1,5 @@
+function myFunc(x = 10) {
+	return x;
+}
+
+console.log(myFunc(5));
---
+++ 17 Spread Operator/index.js
@@ -0,0 +1,5 @@
+var numbers = [1,2,3]; 
+
+var newNumbers = [...numbers, 4,5,6,7,8,9];
+
+console.log(newNumbers);
